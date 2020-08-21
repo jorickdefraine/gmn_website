bbb492828cea32f6b186b57613f74cf4d9764e61
jorickdefraine <defraine.jorick@gmail.com>
2020-08-21 12:46:59 +0200

    update settings.py

--- GuessMyNumber/settings.py
+++ GuessMyNumber/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '%7^b^7(%a9uah2@h9%%e8uyc@si1uc
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['limitless-badlands-05393.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['guessmynumberwithkeras.herokuapp.com','127.0.0.1']
 
 
 # Application definition
